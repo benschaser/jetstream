5fff280239131ec5c55adc9572a84f5a29840c11
benschaser <benschaser@gmail.com>
2025-07-09 13:11:41 -0500

    init and initial navbar config

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import './pages/EditorPage'
+import EditorPage from './pages/EditorPage'
+
+function App() {
+
+  return (
+    <>
+      <EditorPage></EditorPage>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/pages/EditorPage.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './EditorPage.css';
+import '../components/JNavbar';
+import JNavbar from '../components/JNavbar';
+
+function EditorPage() {
+
+  return (
+    <>
+        <JNavbar></JNavbar>
+    </>
+  )
+}
+
+export default EditorPage
